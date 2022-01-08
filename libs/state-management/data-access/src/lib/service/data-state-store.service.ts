3ec5dd274935316cb55980bad7588fa05cae90b8
Ryan Marken <marken.ryan@gmail.com>
2022-01-08 16:36:53 -0500

    Mock Service

--- libs/state-management/data-access/src/lib/service/data-state-store.service.ts
+++ libs/state-management/data-access/src/lib/service/data-state-store.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+export interface DisplayData {
+  loadingStatus: LOADING_STATUS;
+  url: string;
+}
+
+export enum LOADING_STATUS {
+  LOADING = 'LOADING',
+  SUCCESS = 'SUCCESS',
+  ERROR = 'ERROR',
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataStateStoreService {
+  private displayDataBehaviorSubject: BehaviorSubject<DisplayData>;
+  public displayData$: Observable<DisplayData>;
+  private initialDisplayData: DisplayData = {
+    url: '',
+    loadingStatus: LOADING_STATUS.LOADING,
+  };
 
-  constructor() { }
+  constructor() {
+    this.displayDataBehaviorSubject = new BehaviorSubject<DisplayData>(
+      this.initialDisplayData
+    );
+    this.displayData$ = this.displayDataBehaviorSubject.asObservable();
+  }
+
+  public updateLoadingStatus(loadingStatus: LOADING_STATUS): void {
+    this.displayDataBehaviorSubject.next({
+      ...this.getCurrentState(),
+      loadingStatus,
+    });
+  }
+
+  public updateUrl(url: string): void {
+    this.displayDataBehaviorSubject.next({
+      ...this.getCurrentState(),
+      url,
+    });
+  }
+
+  private getCurrentState(): DisplayData {
+    return this.displayDataBehaviorSubject.getValue();
+  }
 }
--- libs/state-management/data-access/src/lib/service/mock-http.service.ts
+++ libs/state-management/data-access/src/lib/service/mock-http.service.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@angular/core';
+import {delay, firstValueFrom, of} from 'rxjs';
 
 @Injectable()
 export class MockHttpService {
+  data: {[key:number]: string} = {
+    1: 'https://runescape.wiki/images/5/56/Frog_%28NPC%29.png?e9771',
+    2: 'https://gamepedia.cursecdn.com/diablo_gamepedia/6/6d/Amazon_Sprite_(Diablo_II).gif?version=a2c43ef34a03447a4d936a5af0d312e9',
+    3: 'https://media.boingboing.net/wp-content/uploads/2015/11/150611A-Sagan.jpg',
+  };
   constructor() {}
+
+  public async callFakeAPI(): Promise<string> {
+    const random = Math.floor(Math.random() * 3) + 1;
+    const url = this.data[random];
+   return await firstValueFrom(of(url)
+      .pipe(
+        delay(1500)));
 }
